def6be525edf4410cefdb25020e12dba896cc7f4
Muhamad Taopik <mr.94t3z@gmail.com>
2024-03-07 03:00:11 +0700

    change the url

--- app/config.ts
+++ app/config.ts
@@ -1,2 +1,2 @@
 // use NODE_ENV to not have to change config based on where it's deployed
-export const NEXT_PUBLIC_URL = process.env.NODE_ENV == "development" ? 'http://localhost:3000' : 'https://kn55hlkl-3000.asse.devtunnels.ms/';
+export const NEXT_PUBLIC_URL = process.env.NODE_ENV == "development" ? 'http://localhost:3000' : 'https://prosocial-heroes-frame.vercel.app/';
